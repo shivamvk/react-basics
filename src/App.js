e317ee1dba924b17efb81f68d24f0ce26c47cd48
shivamvk <shivam.vk529@gmail.com>
2020-05-11 18:30:56 +0530

    child-parent efficient comm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 
 const App = () => {
-  const goals = [
+  const [goals, setGoals] = useState([
     {id: "cg1", title: "Finish the course"},
     {id: "cg2", title: "Learn all about the course main topic"},
     {id: "cg3", title: "Help other students in Q&A"},
-  ];
+  ]);
 
   const addGoalHandler = (newGoal) => {
-    goals.push(newGoal);
-    console.log(goals);
+    //setGoals(goals.concat(newGoal));
+    setGoals(prevGoals => prevGoals.concat(newGoal));
   }
 
   return (
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./NewGoal.css"
 
 const NewGoal = props => {
-
+    const [enteredText, setEnteredText] = useState(''); 
     const addGoalHandler = event => {
         event.preventDefault();
 
         var newGoal = {
             id: Math.random.toString,
-            title: "New course goal"
+            title: enteredText
         }
         props.onAddGoal(newGoal);
     }
 
+    const onTextChangeHandler = event => {
+        setEnteredText(event.target.value);
+    }
+
     return (
         <form className="new-goal" onSubmit={addGoalHandler}>
-            <input type="text" />
+            <input value={enteredText} onChange={onTextChangeHandler} type="text" />
             <button type="submit">Add goal</button>
         </form>
     );
